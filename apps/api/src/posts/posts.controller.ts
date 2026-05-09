3bdde29b38fa8cc5cc9e1273d844a88f9c3d6526
Patryk Barć <108985559+Patrykbarc@users.noreply.github.com>
2026-05-09 23:07:47 +0200

    add mocked get routes

--- apps/api/src/posts/posts.controller.ts
+++ apps/api/src/posts/posts.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+	constructor(private readonly postsService: PostsService) {}
+
+	@Get('/')
+	getAllPosts() {
+		return this.postsService.getAllPosts();
+	}
+
+	@Get('/search')
+	getPostByTitle(@Query('title') title: string) {
+		return this.postsService.getProductByTitle(title);
+	}
+
+	@Get('/:id')
+	getPostById(@Param('id') id: string) {
+		return this.postsService.getProductById(id);
+	}
+}
--- apps/api/src/posts/posts.service.ts
+++ apps/api/src/posts/posts.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+const TODAY = new Date(Date.now());
+const YESTERDAY = new Date(Date.now() - 24 * 60 * 60 * 1000);
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+	private posts = [
+		{
+			id: 1,
+			title: 'First post',
+			createdAt: YESTERDAY,
+			updatedAt: null,
+		},
+		{
+			id: 2,
+			title: 'Second post',
+			createdAt: TODAY,
+			updatedAt: null,
+		},
+	];
+
+	getAllPosts() {
+		return this.posts;
+	}
+
+	getProductById(id: string) {
+		const post = this.posts.find((post) => post.id === +id);
+		if (!post) {
+			throw new NotFoundException('Post not found');
+		}
+
+		return post;
+	}
+
+	getProductByTitle(title: string) {
+		const post = this.posts.find(
+			(post) => post.title.toLocaleLowerCase() === title.toLocaleLowerCase(),
+		);
+		if (!post) {
+			throw new NotFoundException('Post not found');
+		}
+
+		return post;
+	}
+}
